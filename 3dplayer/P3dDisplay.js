bf466a9ffdd70f77256fc50b8421c0a2e0b96381
Paul Slocum <paul.slocum@gmail.com>
2020-03-16 04:16:11 -0700

    Custom geometry proof of concept

--- 3dplayer/P3dDisplay.js
+++ 3dplayer/P3dDisplay.js
@@ -17,6 +17,7 @@ export default class P3dDisplay
     this.camera = new THREE.PerspectiveCamera( 75, windowWidth/windowHeight, 0.1, 1000 );
 
     this.cube = null;
+    this.customCube = null;
 
     this.buildStructures();    
 
@@ -31,6 +32,9 @@ export default class P3dDisplay
 
     this.cube.rotation.x += 0.01;
     this.cube.rotation.y += 0.01;
+
+    this.customCube.rotation.x += 0.01;
+    this.customCube.rotation.y += 0.01;
     
     this.renderer.render( this.scene, this.camera );
   };
@@ -39,25 +43,85 @@ export default class P3dDisplay
   ///////////////////////////////////////////////////////////////////////
   buildStructures()
   {
-    var geometry = new THREE.BoxGeometry();
+    // CAMERA
+    this.camera.position.z = 5;
 
+    // MODEL
+    const width = 4.0;
+    const height = 2.0;
+    const depth = 3.0;
+    
+    // CUSTOM GEOMETRY
+    const customGeometry = new THREE.Geometry();
+    customGeometry.vertices.push(
+      new THREE.Vector3( -1, -1,  1 ),  // 0
+      new THREE.Vector3(  1, -1,  1 ),  // 1
+      new THREE.Vector3( -1,  1,  1 ),  // 2
+      new THREE.Vector3(  1,  1,  1 ),  // 3
+      new THREE.Vector3( -1, -1, -1 ),  // 4
+      new THREE.Vector3(  1, -1, -1 ),  // 5
+      new THREE.Vector3( -1,  1, -1 ),  // 6
+      new THREE.Vector3(  1,  1, -1 ),  // 7
+    );
+
+    /*
+         6----7
+        /|   /|
+       2----3 |
+       | |  | |
+       | 4--|-5
+       |/   |/
+       0----1
+    */
+
+    customGeometry.faces.push(
+       // front
+       new THREE.Face3(0, 3, 2),
+       new THREE.Face3(0, 1, 3),
+       // right
+       new THREE.Face3(1, 7, 3),
+       new THREE.Face3(1, 5, 7),
+       // back
+       new THREE.Face3(5, 6, 7),
+       new THREE.Face3(5, 4, 6),
+       // left
+       new THREE.Face3(4, 2, 6),
+       new THREE.Face3(4, 0, 2),
+       // top
+       new THREE.Face3(2, 7, 6),
+       new THREE.Face3(2, 3, 7),
+       // bottom
+       new THREE.Face3(4, 1, 0),
+       new THREE.Face3(4, 5, 1),
+    );
+
+    //function makeInstance(geometry, color, x) {
+    //const customMaterial = new THREE.MeshBasicMaterial( 0x44FF44 );
+    const customMaterial = new THREE.MeshPhongMaterial();
+    customMaterial.color.setHSL( 1.0, 0, .5);  // ?
+    customMaterial.flatShading = true;			
+    this.customCube = new THREE.Mesh( customGeometry, customMaterial );
+    this.customCube.position.z = -2;
+    this.scene.add( this.customCube );
+
+    
+    // TEST CUBE  
+    var geometry = new THREE.BoxGeometry();
     //const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     const material = new THREE.MeshPhongMaterial();
     //const material = new THREE.MeshStandardMaterial();
-
     material.color.setHSL(0, 1, .5);  // red
     material.flatShading = true;			
     this.cube = new THREE.Mesh( geometry, material );
+    this.scene.add( this.cube );
 
-    // add spotlight for the shadows
+    // SPOTLIGHT
     var spotLight = new THREE.SpotLight(0xffffff);
     spotLight.position.set(-0, 30, 60);
     spotLight.castShadow = true;
     spotLight.intensity = 0.6;
     this.scene.add( spotLight );
 
-    this.scene.add( this.cube );
-    this.camera.position.z = 5;
   }
 
 }
