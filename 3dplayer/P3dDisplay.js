f039662bd107b202e772a0aef773718b1880f5cb
Paul Slocum <paul.slocum@gmail.com>
2020-03-15 18:35:13 -0700

    Geometry construction and update are now in their own class functions

--- 3dplayer/P3dDisplay.js
+++ 3dplayer/P3dDisplay.js
@@ -3,40 +3,28 @@
 export default class P3dDisplay 
 {
 
+
   ///////////////////////////////////////////////////////////////////////
   constructor( windowWidth, windowHeight, renderer ) 
   {
     console.log("---->DISPLAY CONSTRUCTOR");
 
+    this.windowWidth = windowWidth;
+    this.windowHeight = windowHeight;
     this.renderer = renderer;
-
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera( 75, windowWidth/windowHeight, 0.1, 1000 );
 
-    this.geometry = new THREE.BoxGeometry();
+    this.cube = null;
 
-    //const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const material = new THREE.MeshPhongMaterial();
-    //const material = new THREE.MeshStandardMaterial();
+    this.buildStructures();    
 
-    material.color.setHSL(0, 1, .5);  // red
-    material.flatShading = true;			
-    this.cube = new THREE.Mesh( this.geometry, material );
-
-    // add spotlight for the shadows
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-0, 30, 60);
-    spotLight.castShadow = true;
-    spotLight.intensity = 0.6;
-    this.scene.add( spotLight );
-
-    this.scene.add( this.cube );
-
-    this.camera.position.z = 5;
   }
 
+
   ///////////////////////////////////////////////////////////////////////
+  // NOTE: run() MUST BE CALLED EXTERNALLY TO START THE ANIMATION
   run() 
   {
     requestAnimationFrame( this.run.bind(this) );
@@ -46,5 +34,30 @@ export default class P3dDisplay
     
     this.renderer.render( this.scene, this.camera );
   };
+
+
+  ///////////////////////////////////////////////////////////////////////
+  buildStructures()
+  {
+    var geometry = new THREE.BoxGeometry();
+
+    //const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const material = new THREE.MeshPhongMaterial();
+    //const material = new THREE.MeshStandardMaterial();
+
+    material.color.setHSL(0, 1, .5);  // red
+    material.flatShading = true;			
+    this.cube = new THREE.Mesh( geometry, material );
+
+    // add spotlight for the shadows
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-0, 30, 60);
+    spotLight.castShadow = true;
+    spotLight.intensity = 0.6;
+    this.scene.add( spotLight );
+
+    this.scene.add( this.cube );
+    this.camera.position.z = 5;
+  }
 
 }
