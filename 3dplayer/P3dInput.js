f13c7779e8361dbbb7ba14c526832bd854e4f509
Paul Slocum <paul.slocum@gmail.com>
2020-03-19 14:34:35 -0700

    Experimenting with final button layout

--- 3dplayer/P3dInput.js
+++ 3dplayer/P3dInput.js
@@ -7,6 +7,14 @@ export default class P3dInput {
   ///////////////////////////////////////////////////////////////////////
   constructor() 
   {
+    console.log("---->INPUT CLASS CONSTRUCTOR");
+
+    document.addEventListener( 'mousedown', this.mouseDown, false );
+  }
+
+  mouseDown( event )
+  {
+    console.log("---->MOUSE DOWN EVENT");
   }
   
 }
