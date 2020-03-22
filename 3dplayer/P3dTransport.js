53adf6558a648e1accf196d160a852ec02b32635
Paul Slocum <paul.slocum@gmail.com>
2020-03-21 20:02:06 -0700

    Made sound filenames constants

--- 3dplayer/P3dTransport.js
+++ 3dplayer/P3dTransport.js
@@ -8,6 +8,17 @@ import P3dSound from './P3dSound.js'
 //-----------------------------------------------------------------------------------
 
 
+
+export const SoundFilenames = {
+    CLICK_DOWN: '3dplayer/sounds/clickDown.wav',
+    CLICK_UP: '3dplayer/sounds/clickDown.wav',
+    TRAY_OPEN: '3dplayer/sounds/cdDiscOut.wav',
+    TRAY_CLOSE: '3dplayer/sounds/cdDiscIn.wav'
+}
+
+
+
+
 //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 export default class P3dTransport {
 
@@ -18,8 +29,10 @@ export default class P3dTransport {
     console.log("---->TRANSPORT CLASS CONSTRUCTOR");
     
     this.soundPlayer = new P3dSound();
-    this.soundPlayer.loadSound( '3dplayer/sounds/clickDown.wav' );
-    this.soundPlayer.loadSound( '3dplayer/sounds/clickUp.wav' );
+    this.soundPlayer.loadSound( SoundFilenames.CLICK_DOWN );
+    this.soundPlayer.loadSound( SoundFilenames.CLICK_UP );
+    this.soundPlayer.loadSound( SoundFilenames.TRAY_OPEN );
+    this.soundPlayer.loadSound( SoundFilenames.TRAY_CLOSE );
     
   }
   
@@ -29,11 +42,11 @@ export default class P3dTransport {
     switch( buttonEvent )
     {
       case ButtonEvent.BUTTON_DOWN_PLAY: console.log("---->PLAY BUTTON PRESSED (TRANSPORT!)"); 
-            this.soundPlayer.playSound( '3dplayer/sounds/clickDown.wav' ); break;
+            this.soundPlayer.playSound( SoundFilenames.CLICK_DOWN ); break;
       case ButtonEvent.BUTTON_DOWN_PAUSE: console.log("---->PAUSE BUTTON PRESSED (TRANSPORT)"); 
-            this.soundPlayer.playSound( '3dplayer/sounds/clickDown.wav' ); break;
+            this.soundPlayer.playSound( SoundFilenames.CLICK_DOWN ); break;
       case ButtonEvent.BUTTON_UP: console.log("---->BUTTON UP (TRANSPORT)"); 
-            this.soundPlayer.playSound( '3dplayer/sounds/clickUp.wav' ); break;
+            this.soundPlayer.playSound( SoundFilenames.CLICK_UP ); break;
       case ButtonEvent.NONE: console.log("---->NO BUTTON PRESSED (TRANSPORT)"); break;
     }
   }
