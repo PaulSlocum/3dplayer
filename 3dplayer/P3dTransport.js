4ea97ef0ca89a973f28937a4e2590f28b2678495
Paul Slocum <paul.slocum@gmail.com>
2020-03-28 12:26:04 -0700

    Code cleanup

--- 3dplayer/P3dTransport.js
+++ 3dplayer/P3dTransport.js
@@ -86,34 +86,37 @@ export default class P3dTransport {
             }
             break;
       case ButtonEvent.BUTTON_DOWN_PREV: console.log("---->PREV BUTTON PRESSED (TRANSPORT)"); 
-    if( this.trackNumber > 1 )
-    {
-      this.trackNumber--;
-      this.trackPlaying = true;
-      this.musicPlayer.rewindMusic();    
-      this.musicPlayer.playMusic( this.filenameList[ this.trackNumber ] );    
-      this.status = Mode.PLAYING;
-    }
+            this.previousTrack();
+            /*if( this.trackNumber > 1 )
+            {
+              this.trackNumber--;
+              this.trackPlaying = true;
+              this.musicPlayer.rewindMusic();    
+              this.musicPlayer.playMusic( this.filenameList[ this.trackNumber ] );    
+              this.status = Mode.PLAYING;
+            }//*/
             break;
       case ButtonEvent.BUTTON_DOWN_NEXT: console.log("---->NEXT BUTTON PRESSED (TRANSPORT)"); 
-    if( this.trackNumber < this.filenameList.length-1 )
-    {
-      this.trackNumber++;
-      this.trackPlaying = true;
-      this.musicPlayer.rewindMusic();    
-      this.musicPlayer.playMusic( this.filenameList[ this.trackNumber ] );    
-      this.status = Mode.PLAYING;
-    }
+            this.nextTrack();
+            /*if( this.trackNumber < this.filenameList.length-1 )
+            {
+              this.trackNumber++;
+              this.trackPlaying = true;
+              this.musicPlayer.rewindMusic();    
+              this.musicPlayer.playMusic( this.filenameList[ this.trackNumber ] );    
+              this.status = Mode.PLAYING;
+            } //*/
             break;
       case ButtonEvent.BUTTON_DOWN_FAST_FORWARD: console.log("---->FAST FORWARD BUTTON PRESSED (TRANSPORT)"); 
             break;
       case ButtonEvent.BUTTON_DOWN_REWIND: console.log("---->REWIND BUTTON PRESSED (TRANSPORT)"); 
             break;
       case ButtonEvent.BUTTON_DOWN_STOP: console.log("---->STOP BUTTON PRESSED (TRANSPORT)"); 
-    this.trackPlaying = false;
-    this.musicPlayer.rewindMusic();    
-    this.trackNumber = 1;
-    this.status = Mode.STOPPED;
+            this.stop();
+            /*this.trackPlaying = false;
+            this.musicPlayer.rewindMusic();    
+            this.trackNumber = 1;
+            this.status = Mode.STOPPED; //*/
             break;
       case ButtonEvent.BUTTON_DOWN_STANDBY: console.log("---->STANDBY BUTTON PRESSED (TRANSPORT)"); 
             this.trackPlaying = false;
